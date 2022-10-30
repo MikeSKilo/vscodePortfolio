cd523e579d43eb60a4a3e489150a9c3b5979b6ce
Min Su Kim <tominsukim@hotmail.com>
2022-10-30 08:05:38 -0700

    changed favicon

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -52,7 +52,7 @@ export default function Home({ setSelectedIndex }: Props) {
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
               <Typography variant="subtitle1" gutterBottom>
-                Always difficult, always beautiful
+                Hello, World.
                 {/* Better an{' '}
                 <Box fontWeight="fontWeightMedium" display="inline">
                   oops
